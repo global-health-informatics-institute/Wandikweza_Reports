d3e424b9a57f1b64df4043287f5d5c8529069d25
joanamhone <joanamhone@gmail.com>
2024-10-22 08:45:44 +0200

    turbo frames

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,28 +1,48 @@
 class MainController < ApplicationController
   def index
     @selected_report = params['report_type']
+
     case @selected_report
     when 'Daily Cash'
-      daily_summary_report
+      @report_content = daily_summary_report
+      partial = 'daily_summary_report'
     when 'Cashier'
-      cashier_summary_report
+      @report_content = cashier_summary_report
+      partial = 'cashier_summary_report'
     when 'Income'
-      income_summary_report
+      @report_content = income_summary_report
+      partial = 'income_summary_report'
     when 'Census'
-      census_report
+      @report_content = census_report
+      partial = 'census_report'
+    else
+      @report_content = nil
+      partial = 'default_report'
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("report_content", partial: partial, locals: { report: @report_content })
+      end
+      format.html
     end
   end
 
-  def show
-
-  end
+  private
 
   def daily_summary_report
+    # Your logic for generating the daily summary report
   end
 
-  def create
+  def cashier_summary_report
+    # Your logic for generating the cashier summary report
   end
 
-  def destroy
+  def income_summary_report
+    # Your logic for generating the income summary report
+  end
+
+  def census_report
+    # Your logic for generating the census report
   end
 end
